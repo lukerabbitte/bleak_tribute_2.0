9d7718865964ef0d2dc2ebda70c25b905379612e
lukerabbitte <luke.rabbitte@gmail.com>
2024-07-10 02:53:06 +0100

    More

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,12 @@
 <script>
   import './styles.css';
+  import { base } from '$app/paths';
 </script>
 
 <nav>
-	<a href="/">Home</a>
-    <a href="https://github.com/lukerabbitte">GitHub</a>
-    <a href="https://www.linkedin.com/in/lukerabbitte/">LinkedIn</a>
+	<a href="{base}/">Home</a>
+    <a href="{base}https://github.com/lukerabbitte">GitHub</a>
+    <a href="{base}https://www.linkedin.com/in/lukerabbitte/">LinkedIn</a>
     <a href="mailto:luke.rabbitte@gmail.com">Email</a>
 </nav>
 
